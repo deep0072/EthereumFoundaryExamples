7649aae280f1ec57506ccb198c22c1f3d7b5a249
unknown <deepakumar50136@gmail.com>
2022-07-07 01:40:55 +0530

    commenting

--- foundary_example/test/Contract.t.sol
+++ foundary_example/test/Contract.t.sol
@@ -16,6 +16,6 @@ contract ContractTest is Test {
         counter.incrementCount();
         int256 value = counter.getCount();
         assertEq(value, 21);
-        emit log_named_int("testGetCount", value); // to get the value in the test log then run forge test -vv
+        emit log_named_int("testGetCount", value); // to get the value in cmd terminal then run forge test -vv
     }
 }
