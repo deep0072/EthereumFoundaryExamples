61f47fa98460aafadb66201ea15a08c5e5345cfb
unknown <deepakumar50136@gmail.com>
2022-07-07 13:59:12 +0530

    update info in read me about deploying and calling contract

--- foundary_example/script/Contract.s.sol
+++ foundary_example/script/Contract.s.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
+import {Counter} from "src/Counter.sol";
+
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        new Counter(10);
+        vm.stopBroadcast();
     }
 }
