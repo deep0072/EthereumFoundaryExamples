e81d0d16346db111dd3ab5d91d54946d44b6f8f2
deep0072 <deepakumar50136@gmail.com>
2023-06-26 22:22:00 +0530

    testcases for fundme

--- Foundry-Fundme/src/FundMe.sol
+++ Foundry-Fundme/src/FundMe.sol
@@ -11,20 +11,21 @@ contract FundMe {
 
     uint256 public constant MINIMUM_USD = 5e18;
 
-    address[] public funders;
+    address[] public s_funders;
 
-    mapping(address => uint256) public amountToFunded;
+    mapping(address => uint256) public s_amountToFunded;
 
-    address public immutable owner;
+    address public immutable s_owner;
     AggregatorV3Interface private s_priceFeed;
+    uint256 ethPriceUsd;
 
     constructor(address priceFeed) {
-        owner = msg.sender;
+        s_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     modifier onlyOwner() {
-        if (msg.sender != owner) {
+        if (msg.sender != s_owner) {
             revert UnAuthorised();
         }
         _;
@@ -37,25 +38,53 @@ contract FundMe {
     }
 
     function fund() public payable {
-        if (msg.value.conversionRate(s_priceFeed) < MINIMUM_USD) {
+        ethPriceUsd = msg.value.conversionRate(s_priceFeed);
+        if (ethPriceUsd < MINIMUM_USD) {
             revert Not_Sufficient_Amount();
         }
 
-        funders.push(msg.sender);
-        amountToFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_amountToFunded[msg.sender] += msg.value;
     }
 
     function withDraw() public onlyOwner {
-        for (uint256 funderIndex; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            amountToFunded[funder] = 0;
+        for (
+            uint256 funderIndex;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_amountToFunded[funder] = 0;
         }
 
-        funders = new address[](0);
+        s_funders = new address[](0);
 
-        (bool success,) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
 
         require(success, "txn failed");
+    }
+
+    function getAddressAmountToFunded(address _funder)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 amount = s_amountToFunded[_funder];
+        return amount;
+    }
+
+    function getFunderWithIndex(uint256 funderIndex)
+        public
+        view
+        returns (address)
+    {
+        return s_funders[funderIndex];
+    }
+
+    function getEthPriceUsd() public view returns (uint256) {
+        return ethPriceUsd;
     }
 
     receive() external payable {
--- Foundry-Fundme/test/FundMe.t.sol
+++ Foundry-Fundme/test/FundMe.t.sol
@@ -8,6 +8,11 @@ import {FundMeScript} from "../script/Fundme.s.sol";
 contract FundMeTest is Test {
     // public variable is fundMe contract
     FundMe public fundMe;
+    // makeAddr function used to create random address
+
+    address User = makeAddr("Deepak"); // User is the funder here
+    uint256 constant SEND_VALUE = 1 ether; // amount to fund the contract
+    uint256 constant STARTING_BALANCE = 10e18; // amount to fund the contract
 
     // setUp function runs always first and deploy contract.
     function setUp() external {
@@ -24,13 +29,31 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        console.log(fundMe.owner());
+        console.log(fundMe.s_owner());
         console.log(address(this));
-        assertEq(fundMe.owner(), msg.sender);
+        assertEq(fundMe.s_owner(), msg.sender);
     }
 
     function testPriceFeedversionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        // this test supposed to be passed when we don't have enough eth to
+        vm.expectRevert(); // the next line should revert
+
+        fundMe.fund(); // here we are trying to send the zero eth to
+    }
+
+    function testFundUpdatesDataStructure() public {
+        vm.deal(User, STARTING_BALANCE); // deal method set address to new balance
+        console.log(User.balance, "balance");
+        vm.prank(User); // set the msg.sender means the code below will be executed by "User"
+        fundMe.fund{value: SEND_VALUE}();
+        console.log(address(fundMe).balance, "balance");
+
+        uint256 amount = fundMe.getAddressAmountToFunded(User);
+        assertEq(amount, SEND_VALUE);
     }
 }
