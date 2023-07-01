da246550c80c8b599c6d92cf554cfd75d7ad1f10
deep0072 <deepakumar50136@gmail.com>
2023-07-01 18:55:31 +0530

    added script to interact with deployed contract

--- Foundry-Fundme/script/Fundme.s.sol
+++ Foundry-Fundme/script/Fundme.s.sol
@@ -10,11 +10,12 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 contract FundMeScript is Script, Test {
     function run() public returns (FundMe) {
         // declaring "helperConfig" before startBroadcasting so that it will not cost any gas
+        // this instance let us to ge the priceFedd address based on chainId
         HelperConfig helperConfig = new HelperConfig();
         address ethUsdPriceFeed = helperConfig.ActivateConfig();
         console.log(ethUsdPriceFeed, "eth price usd");
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed); // this will deploy the contract
         vm.stopBroadcast();
         return fundMe;
     }
--- Foundry-Fundme/script/Interactions.s.sol
+++ Foundry-Fundme/script/Interactions.s.sol
@@ -11,7 +11,9 @@ contract FundFundMe is Script {
 
     function fundfundMe(address mostRecentDeployment) public {
         vm.startBroadcast();
+        // here we are connected to fund me contract and sending fund to contract
         FundMe(payable(mostRecentDeployment)).fund{value: SEND_VALUE}();
+
         vm.stopBroadcast();
 
         console.log("funded amount is %s", SEND_VALUE);
@@ -19,8 +21,10 @@ contract FundFundMe is Script {
 
     function run() external {
         // grab the address of most recent deployed contract here
-        address mostRecentlyDeployed =
-            DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
 
         fundfundMe(mostRecentlyDeployed);
     }
@@ -30,16 +34,20 @@ contract WithdrawFundMe is Script {
     uint256 constant SEND_VALUE = 1 ether;
 
     function withdrawFundMe(address mostRecentDeployment) public {
+        vm.startBroadcast();
+        // here we are connected to deployed contract and trying to withdraw amount
         FundMe(payable(mostRecentDeployment)).withDraw();
+
+        vm.stopBroadcast();
     }
 
     function run() external {
         // grab the address of most recent deployed contract here
-        address mostRecentlyDeployed =
-            DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        vm.startBroadcast();
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
 
         withdrawFundMe(mostRecentlyDeployed);
-        vm.stopBroadcast();
     }
 }
